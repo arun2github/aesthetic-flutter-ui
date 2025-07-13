9afe9e623ec177bb081eb4ac461a81d3fc604d83
arun2github <anandarun70@gmail.com>
2025-07-13 20:52:03 +0530

    Fix: Remove build/ and .dart_tool/ from Git tracking

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Prem/Desktop/projects/instagram_ui_showcase/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/Prem/Desktop/projects/instagram_ui_showcase/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
